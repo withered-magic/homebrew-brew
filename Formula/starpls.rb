57dd187f49333fbf02f32a158ea402c7aa4adfc3
withered-magic <withering.magic@gmail.com>
2024-04-13 13:33:45 -0700

    Update to v0.1.11

--- Formula/starpls.rb
+++ Formula/starpls.rb
@@ -1,11 +1,11 @@
 class Starpls < Formula
   desc "A language server for Starlark, the configuration language used by Bazel and Buck2."
   homepage "https://github.com/withered-magic/starpls"
-  version "0.1.10"
+  version "0.1.11"
 
   if OS.mac?
     if Hardware::CPU.arm?
-      url "https://github.com/withered-magic/starpls/releases/download/v0.1.10/starpls-darwin-arm64.tar.gz"
+      url "https://github.com/withered-magic/starpls/releases/download/v0.1.11/starpls-darwin-arm64.tar.gz"
     else
       raise "Unsupported MacOS architecture, please follow the manual installation instructions at https://github.com/withered-magic/starpls."
     end

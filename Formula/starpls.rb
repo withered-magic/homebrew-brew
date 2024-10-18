bc21f6e6e931f81248ecd8411b60c2d6259474fe
withered-magic <withering.magic@gmail.com>
2024-10-17 22:32:03 -0700

    chore: update to starpls v0.1.15

--- Formula/starpls.rb
+++ Formula/starpls.rb
@@ -1,19 +1,19 @@
 class Starpls < Formula
-  desc "A language server for Starlark, the configuration language used by Bazel and Buck2."
-  homepage "https://github.com/withered-magic/starpls"
-  version "0.1.14"
+  desc 'A language server for Starlark, the configuration language used by Bazel and Buck2.'
+  homepage 'https://github.com/withered-magic/starpls'
+  version '0.1.15'
 
   if OS.mac?
     if Hardware::CPU.arm?
-      url "https://github.com/withered-magic/starpls/releases/download/v0.1.14/starpls-darwin-arm64.tar.gz"
+      url 'https://github.com/withered-magic/starpls/releases/download/v0.1.15/starpls-darwin-arm64.tar.gz'
     else
-      raise "Unsupported MacOS architecture, please follow the manual installation instructions at https://github.com/withered-magic/starpls."
+      raise 'Unsupported MacOS architecture, please follow the manual installation instructions at https://github.com/withered-magic/starpls.'
     end
   else
-    raise "Unsupported OS, please follow the manual installation instructions at https://github.com/withered-magic/starpls."
+    raise 'Unsupported OS, please follow the manual installation instructions at https://github.com/withered-magic/starpls.'
   end
 
   def install
-    bin.install "starpls"
+    bin.install 'starpls'
   end
 end
